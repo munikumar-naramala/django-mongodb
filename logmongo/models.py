bbc47588e5b98e0feca3b53e7a6d378706fc58a4
Eficens User <thrishithakalvakota@gmail.com>
2023-03-06 10:09:54 +0530

    mongodb

---
+++ logmongo/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Logmongo(models.Model):
+    objects = None
+    title = models.CharField(max_length=70, blank=False, default='')
+    description = models.CharField(max_length=200,blank=False, default='')
+    published = models.BooleanField(default=False)
